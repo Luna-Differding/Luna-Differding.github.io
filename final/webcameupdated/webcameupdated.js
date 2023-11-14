77dafd573565633099ba8ab3f294a7c8d2badc22
Luna-Differding <luna.differding.portales@gmail.com>
2023-11-14 15:50:35 -0800

    updated

---
+++ final/webcameupdated/webcameupdated.js
@@ -0,0 +1,14 @@
+let capture;
+
+function setup() {
+  createCanvas(640, 480);
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
+}
+
+function draw() {
+  background(255);
+  image(capture, 0, 0, 640, 480);
+  //filter(INVERT);
+}
