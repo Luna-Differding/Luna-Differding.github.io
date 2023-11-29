18508b2f0e2cea68819d8fbf9c5b234587f7a193
Luna-Differding <luna.differding.portales@gmail.com>
2023-11-28 16:24:25 -0800

    update 1

--- final/webcam_blur_/webcam_blur_.js
+++ final/webcam_blur_/webcam_blur_.js
@@ -3,12 +3,12 @@ let capture;
 function setup() {
   createCanvas(640, 480);
   capture = createCapture(VIDEO);
-  capture.size(640, 480);
+  capture.size(320, 240);
   capture.hide();
 }
 
 function draw() {
   background(0);
   image(capture, 0, 0, 640, 480);
-  //filter(INVERT);
+  filter(BLUR, 1.5);
 }
