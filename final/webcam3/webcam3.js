1ed3569cc694dac0b26ddbcea28454153c008209
Luna-Differding <luna.differding.portales@gmail.com>
2023-11-28 17:28:07 -0800

    push

---
+++ final/webcam3/webcam3.js
@@ -0,0 +1,31 @@
+
+let theShader;
+let cam;
+
+function preload(){
+  // load the shader
+  theShader = loadShader('webcam.vert', 'webcam.frag');
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+  
+  cam = createCapture(VIDEO);
+  cam.size(windowWidth, windowHeight);
+  
+  cam.hide();
+}
+
+function draw() {
+  // shader() sets the active shader with our shader
+  shader(theShader);
+  
+  // passing cam as a texture
+  theShader.setUniform('tex0', cam);
+
+  // rect gives us some geometry on the screen
+  rect(0,0,width,height);
+  
+}
